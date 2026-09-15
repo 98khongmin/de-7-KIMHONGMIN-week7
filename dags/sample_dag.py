f197b1e7f76c2194f7761bb1a4880c905827f272
98khongmin <98khongmin@gmail.com>
2026-09-15 13:16:12 +0900

    Q3: 프로그램 오류

--- dags/sample_dag.py
+++ dags/sample_dag.py
@@ -15,7 +15,7 @@ with DAG(
         "start_date": datetime(2025, 1, 1)
     },
     schedule="0 0 * * *",
-    catchup=False,
+    catchup=true,
 ) as dag:
 
     start = EmptyOperator(task_id="start")
